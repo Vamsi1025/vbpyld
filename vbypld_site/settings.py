01f6fb604b124d684bda4fb6f8d6fe1f84d7f411
theppl <polimera0000@gmail.com>
2026-07-30 12:00:59 +0530

    Configure PostgreSQL for Railway

--- vbypld_site/settings.py
+++ vbypld_site/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,11 +53,17 @@ TEMPLATES = [
 WSGI_APPLICATION = "vbypld_site.wsgi.application"
 
 
-DATABASES = {
+'''DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+}'''
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
